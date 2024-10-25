08e58dbb22590aa30cda83b0caef2242cb81dbb6
Aquabet <jiaxianli57@gmail.com>
2024-10-25 15:59:33 -0700

    New feature: change database from local to AWS RDS

--- provisioners/updateOs.sh
+++ provisioners/updateOs.sh
@@ -8,8 +8,4 @@ sudo apt-get update
 
 sudo apt-get upgrade -y
 
-sudo apt-get install -y --fix-missing unzip mysql-server python3 python3-pip python3-venv yq
-
-sudo systemctl start mysql
-
-sudo systemctl enable mysql
+sudo apt-get install -y --fix-missing unzip python3 python3-pip python3-venv yq
