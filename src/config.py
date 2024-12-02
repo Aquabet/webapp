5ed57caadb14f1e4c180e34f032cf999d209073c
Aquabet <jiaxianli57@gmail.com>
2024-12-02 05:58:50 -0800

    fix bug

--- src/config.py
+++ src/config.py
@@ -5,31 +5,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def load_secrets(secret_name):
+    try:
+        client = boto3.client('secretsmanager', region_name=os.getenv('AWS_REGION'))
+        response = client.get_secret_value(SecretId=secret_name)
+        if 'SecretString' in response:
+            return json.loads(response['SecretString'])
+        else:
+            raise ValueError("Secrets not in expected format")
+    except Exception as e:
+        print(f"Error fetching secrets from Secrets Manager: {e}")
+        return {}
+    
+def set_sqlalchemy_database_uri():
+    db_secrets = load_secrets(os.getenv('DB_SECRETS_NAME'))
+    db_username = db_secrets.get('username')
+    db_password = db_secrets.get('password')
+    db_host = os.getenv('RDS_ENDPOINT')
+    db_name = os.getenv('DB_NAME')
+    url = str(f"mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}")
+    return url
+
 class Config:
-
-    def load_secrets(self, secret_name):
-        try:
-            client = boto3.client('secretsmanager', region_name=os.getenv('AWS_REGION'))
-            response = client.get_secret_value(SecretId=secret_name)
-            if 'SecretString' in response:
-                return json.loads(response['SecretString'])
-            else:
-                raise ValueError("Secrets not in expected format")
-        except Exception as e:
-            print(f"Error fetching secrets from Secrets Manager: {e}")
-            return {}
-
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        db_secrets = self.load_secrets(os.getenv('DB_SECRETS_NAME'))
-        db_username = db_secrets.get('username')
-        db_password = db_secrets.get('password')
-        db_host = os.getenv('RDS_ENDPOINT')
-        db_name = os.getenv('DB_NAME')
-
-        return f"mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}"
-        
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = set_sqlalchemy_database_uri()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
     S3_ENDPOINT_URL = os.getenv('S3_ENDPOINT_URL')
